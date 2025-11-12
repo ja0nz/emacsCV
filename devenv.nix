5014eb57f1a40f1dfbf7ca6d52b4dfaf3b3255bf
ja0nz <git@ja.nz>
2025-11-12 22:25:22 +0100

    ci: Added some helpers and build pipeline

--- devenv.nix
+++ devenv.nix
@@ -7,14 +7,39 @@
   # https://devenv.sh/languages/
   languages.texlive.enable = true;
 
-  # https://devenv.sh/scripts/
-  # 
-  scripts = {
-    # Usage: org2tex cv/*.org
-    # Usage: org2tex
-    "org2tex" = {
-      exec = "emacs --batch -Q --load $DEVENV_ROOT/export-cv.el $1";
+  # https://devenv.sh/tasks/
+  # Usage: devenv tasks run build
+  tasks = {
+    "build:tex" = {
+      exec = "emacs --batch -Q --load $DEVENV_ROOT/export-cv.el";
       description = "Build from org-mode to tex";
+    };
+    "build:pdf" = {
+      exec = "tectonic -X build";
+      after = [ "build:tex" ];
+      description = "Build from tex to pdf";
+    };
+  };
+
+  # https://devenv.sh/scripts/
+  scripts = {
+    # Usage: o2t *.org
+    # Usage: o2t
+    "o2t" = {
+      exec = "emacs --batch -Q --load $DEVENV_ROOT/export-cv.el $1";
+      description = "Quick: Build from org-mode to tex";
+    };
+    # Usage: t2p *.tex 
+    "t2p" = {
+      exec = ''
+        set -e
+        input="$1"
+        base=$(basename "$input" .tex)
+        output_dir="$DEVENV_ROOT/build/$base"
+        mkdir -p "$output_dir"
+        tectonic "$input" -o "$output_dir"
+      '';
+      description = "Quick: Build from tex to pdf";
     };
   };
 }
